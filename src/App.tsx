c9694fb2160b0ba848d8800b7c3f7b90cd53fbf4
Vishwak13 <vishwak.k@demandvibes.com>
2025-11-25 18:29:16 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 export type Page = 'home' | 'login' | 'signup' | 'dashboard';
 
@@ -11,6 +12,8 @@ function App() {
     switch (currentPage) {
       case 'login':
         return <Login onNavigate={setCurrentPage} />;
+      case 'signup':
+        return <Signup onNavigate={setCurrentPage} />;
       default:
         return <Home onNavigate={setCurrentPage} />;
     }
